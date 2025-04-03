ca8af11867afe7387089abe47c231334ab2f4cec
zll600 <3400692417@qq.com>
2025-04-03 22:00:29 +0800

    complete chapter1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop{};
 }
