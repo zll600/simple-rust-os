561b676c6242f2a7ac40e638f98849f70e65a944
zll600 <3400692417@qq.com>
2025-04-04 20:41:31 +0800

    complete chapter8

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,10 +1,12 @@
 use pic8259::ChainedPics;
+use x86_64::structures::idt::PageFaultErrorCode;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
 
 use crate::print;
 use crate::println;
 use crate::gdt;
+use crate::hlt_loop;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -24,6 +26,7 @@ lazy_static! {
             timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(
             keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -109,4 +112,17 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode)
+{
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
